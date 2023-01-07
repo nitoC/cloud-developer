fdafb09abb39595f4fc1a2aecd66923a746fbf35
nitoc <daren2track@gmail.con>
2023-01-07 14:05:27 +0100

    refactor(server.ts): add types to req and res param

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -33,12 +33,18 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  interface reqType extends Request{
+    [key:string]:any
+  }
+  interface resType extends Response{
+    [key:string]:any
+  }
+  app.get( "/", async ( req:reqType, res:resType ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
 
   //filter images and sends to view
-  app.get( "/filteredimage", async ( req, res ) => {
+  app.get( "/filteredimage", async ( req:reqType, res:resType ) => {
     
     const image_url  = req.query.image_url as string ;
        
